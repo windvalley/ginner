1cd148fa1c4b8e598bbaf8176837a3439773c543
windvalley <i@sre.im>
2020-08-04 15:14:53 +0800

    Remove cmd lock file when ctrl+c or kill.

--- cmd/command1/main.go
+++ cmd/command1/main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
 
+	"github.com/spf13/pflag"
+
+	"use-gin/config"
 	"use-gin/logger"
 	"use-gin/utils"
 )
 
 func main() {
+	cfg := pflag.StringP("config", "c", "", "Specify your configuration file")
+	pflag.Parse()
+	if *cfg == "" {
+		binName := filepath.Base(os.Args[0])
+		fmt.Printf("missing parameter\nUsage of %s:\n  -c, --config string"+
+			"   Specify your configuration file\n", binName)
+		os.Exit(2)
+	}
+
+	config.ParseConfig(*cfg)
+	logger.InitCMDLog()
+
+	// process lock
 	lock, lockFile, err := utils.ProcessLock()
 	if err != nil {
 		logger.Log.Fatal(err)
@@ -16,5 +35,19 @@ func main() {
 	defer os.Remove(lockFile)
 	defer lock.Close()
 
-	fmt.Println("vim-go")
+	//ctx, cancel := context.WithCancel(context.Background())
+	//go xxx(ctx)
+
+	sigC := make(chan os.Signal)
+	signal.Notify(sigC)
+	for sig := range sigC {
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT,
+			syscall.SIGKILL:
+			logger.Log.Infof("%v signal captured, quit.", sig)
+			//cancel()
+			os.Remove(lockFile)
+			os.Exit(1)
+		}
+	}
 }
