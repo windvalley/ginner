a6030e0bc76afe4ccf7dc1250193d6ecc71ea1ad
windvalley <i@sre.im>
2020-08-23 15:14:19 +0800

    Middleware: jwt bug fix and code optimization.

--- midware/jwt.go
+++ midware/jwt.go
@@ -1,6 +1,8 @@
 package midware
 
 import (
+	"strings"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 
@@ -14,6 +16,26 @@ func JWT() gin.HandlerFunc {
 		jwtToken := c.Query("jwt")
 
 		if jwtToken == "" {
+			jwtToken = c.Query("token")
+		}
+
+		if jwtToken == "" {
+			authHeader := c.GetHeader("Authorization")
+			if authHeader != "" {
+				values := strings.Split(authHeader, " ")
+				if len(values) != 2 || values[0] != "Bearer" {
+					err1 := errcode.New(errcode.TokenInvalidError, nil)
+					err1.Add("Request header 'Authorization' format invalid.")
+					handler.SendResponse(c, err1, nil)
+
+					c.Abort()
+					return
+				}
+				jwtToken = values[1]
+			}
+		}
+
+		if jwtToken == "" {
 			err1 := errcode.New(errcode.TokenInvalidError, nil)
 			err1.Add("no JWT(Json Web Token) found.")
 			handler.SendResponse(c, err1, nil)
@@ -21,7 +43,8 @@ func JWT() gin.HandlerFunc {
 			c.Abort()
 			return
 		} else {
-			if _, err := auth.ParseJWT(jwtToken); err != nil {
+			claims, err := auth.ParseJWT(jwtToken)
+			if err != nil {
 				switch err.(*jwt.ValidationError).Errors {
 				case jwt.ValidationErrorExpired:
 					err1 := errcode.New(errcode.TokenInvalidError, err)
@@ -40,6 +63,9 @@ func JWT() gin.HandlerFunc {
 				c.Abort()
 				return
 			}
+
+			// NOTE: handler function can be get username by c.Get("username")
+			c.Set("username", claims.Username)
 		}
 
 		c.Next()
