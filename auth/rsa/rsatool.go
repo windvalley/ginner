23814bc4d3e03573f46001881ae385acb5a5765f
windvalley <i@sre.im>
2020-08-24 22:31:04 +0800

    Add generate rsa key pairs tool.

---
+++ auth/rsa/rsatool.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+)
+
+func main() {
+	genRsaKeyPairs(1024)
+}
+
+// generate private.pem and public.pem
+func genRsaKeyPairs(bits int) error {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derStream,
+	}
+	file, err := os.Create("private.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+
+	publicKey := &privateKey.PublicKey
+	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derPkix,
+	}
+
+	file, err = os.Create("public.pem")
+	if err != nil {
+		return err
+	}
+
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
