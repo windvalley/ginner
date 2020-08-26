3b2c285d5102189fe1801aa6d702954970335954
windvalley <i@sre.im>
2020-08-26 19:30:44 +0800

    Update base model.

--- model/rdb/basemodel.go
+++ model/rdb/basemodel.go
@@ -4,7 +4,7 @@ import "time"
 
 type Model struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time  `json:"create_at" time_format:"2006-01-02"`
-	UpdatedAt time.Time  `json:"update_at" time_format:"2006-01-02"`
-	DeletedAt *time.Time `sql:"index" json:"delete_at" time_format:"2006-01-02"`
+	CreatedAt time.Time  `json:"create_at" time_format:"2006-01-02 15:04:05"`
+	UpdatedAt time.Time  `json:"update_at" time_format:"2006-01-02 15:04:05"`
+	DeletedAt *time.Time `sql:"index" json:"delete_at" time_format:"2006-01-02 15:04:05"`
 }
