b28197ea2579b40b5ab2d98271192a98de5e115d
windvalley <i@sre.im>
2020-11-12 19:59:07 +0800

    Util: add tools related to IP

---
+++ util/ip.go
@@ -0,0 +1,55 @@
+package util
+
+import (
+	"fmt"
+	"net"
+)
+
+var privateIPBlocks []*net.IPNet
+
+func init() {
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"169.254.0.0/16", // RFC3927 link-local
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+		"fc00::/7",       // IPv6 unique local addr
+	} {
+		_, block, err := net.ParseCIDR(cidr)
+		if err != nil {
+			panic(fmt.Errorf("parse error on %q: %v", cidr, err))
+		}
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+}
+
+// IsIP determine whether the given string is an IP
+func IsIP(ipaddr string) bool {
+	if net.ParseIP(ipaddr) == nil {
+		return false
+	}
+	return true
+}
+
+// IsPrivateIP determine whether the given string is an intranet IP
+func IsPrivateIP(ipaddr string) bool {
+	ip := net.ParseIP(ipaddr)
+	if ip == nil {
+		return false
+	}
+
+	if ip.IsLoopback() || ip.IsLinkLocalUnicast() || ip.IsLinkLocalMulticast() {
+		return true
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+
+	return false
+}
