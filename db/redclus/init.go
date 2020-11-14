1e5739d64de35bd5ce57b0535cd5f4cbf388036e
windvalley <i@sre.im>
2020-11-14 18:54:29 +0800

    Util: implement redis distributed lock tool

--- db/redclus/init.go
+++ db/redclus/init.go
@@ -1,6 +1,8 @@
 package redclus
 
 import (
+	"time"
+
 	"github.com/go-redis/redis"
 
 	"ginner/config"
@@ -17,4 +19,19 @@ func Init() {
 		PoolSize:    config.Conf().RedisCluster.PoolSize,
 		PoolTimeout: config.Conf().RedisCluster.PoolTimeout,
 	})
+}
+
+// Get redis get
+func Get(key string) (string, error) {
+	return Redis.Get(key).Result()
+}
+
+// Set redis set
+func Set(key string, value interface{}, expiration int) (string, error) {
+	return Redis.Set(key, value, time.Duration(expiration)).Result()
+}
+
+// Del redis del
+func Del(key string) (int64, error) {
+	return Redis.Del(key).Result()
 }
---
+++ util/redismutex.go
@@ -0,0 +1,103 @@
+package util
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis"
+
+	"ginner/db/redclus"
+	"ginner/logger"
+)
+
+// RedisMutex redis distributed lock object
+type RedisMutex struct {
+	Redis *redis.ClusterClient
+	// redis key
+	MutexName string
+	// redis value
+	ValueUUID string
+	// key expiration seconds
+	Expiration time.Duration
+	// task timeout seconds
+	Timeout time.Duration
+	// delete the redis key while get an unlock signal
+	UnlockC chan int
+	Err     error
+}
+
+// NewRedisMutex get a new redis mutex with given name and other parameters
+func NewRedisMutex(mutexName string, expiration,
+	timeout time.Duration) *RedisMutex {
+
+	uuid, err := GenerateUUID()
+	c := make(chan int)
+	return &RedisMutex{
+		Redis:      redclus.Redis,
+		MutexName:  mutexName,
+		ValueUUID:  uuid,
+		Expiration: expiration,
+		Timeout:    timeout,
+		UnlockC:    c,
+		Err:        err,
+	}
+}
+
+// Lock locker lock
+func (m *RedisMutex) Lock() error {
+	locker := m.MutexName + ":" + m.ValueUUID
+	ret := m.Redis.SetNX(m.MutexName, m.ValueUUID, m.Expiration*time.Second)
+	if !ret.Val() {
+		return fmt.Errorf("%s locked", m.MutexName)
+	}
+
+	timeoutC := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-time.After(m.Timeout * time.Second):
+				timeoutC <- true
+				return
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-time.After((m.Expiration - 10) * time.Second):
+				ret := m.Redis.SetXX(m.MutexName, m.ValueUUID, m.Expiration*time.Second)
+				if !ret.Val() {
+					logger.Log.Warnf("locker %s renewal failed: %s", locker, ret.Err())
+				}
+			case <-m.UnlockC:
+				logger.Log.Debugf("locker %s unlocked, so stop renewval", locker)
+				return
+			case <-timeoutC:
+				logger.Log.Warnf("locker %s timeout, so stop renewval", locker)
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+// Unlock locker unlock
+func (m *RedisMutex) Unlock() error {
+	m.UnlockC <- 1
+	locker := m.MutexName + ":" + m.ValueUUID
+	value, err := redclus.Get(m.MutexName)
+	if err != nil || value == "" {
+		return fmt.Errorf("%s unlock error: %v", locker, err)
+	}
+	if value != m.ValueUUID {
+		return fmt.Errorf("locker '%s' is not owned by the current task", locker)
+	}
+
+	if _, err = redclus.Del(m.MutexName); err != nil {
+		return fmt.Errorf("%s unlock error: %v", locker, err)
+	}
+
+	return nil
+}
