a7df4041de0045ace29ea47d3ea94f98c2e31a22
windvalley <i@sre.im>
2021-01-29 13:40:33 +0800

    Deployment: service.sh optimization

--- service.sh
+++ service.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+# service.sh
+#
 # Start gin and graceful stop or restart gin.
-#
-# Mind that execute `export RUNENV=dev` first in development environment,
-# or execute `export RUNENV=prod` first in production environment.
-#
-# Or execute as:
-# RUNENV=<dev|prod> ./service.sh <start|stop|reload|restart|status>
 
 
 PROJECT_PATH="./"
 BIN_NAME="ginner"
+COMMAND="./$BIN_NAME -c conf/dev.config.toml"
 PID_FILE=$PROJECT_PATH/logs/${BIN_NAME}.pid
 PID=$(cat $PID_FILE 2>/dev/null)
+STATUS=
 
 
 usage(){
@@ -22,43 +20,50 @@ usage(){
 }
 
 status(){
-    [[ -z "$PID" ]] && {
-        echo "$BIN_NAME was stopped"
-        return
-    }
-
     # shellcheck disable=SC2009
-    ps -ef | grep -v grep | awk '{print $2}' | grep -wq "$PID" &&
-        echo "$BIN_NAME is running, pid $PID" ||
-           echo "$BIN_NAME was stopped"
+    ps -ef | grep -v grep | grep -q "$COMMAND" && {
+        STATUS=running
+        echo "$BIN_NAME is running, pid $PID" 
+        return
+    } 
+
+    STATUS=stopped
+    echo "$BIN_NAME was stopped"
 }
 
 start(){
-    status | grep -q running && {
-        echo "error: $BIN_NAME is already running, do not start again."
+    [[ "$STATUS" = "running" ]] && {
+        echo "warning: $BIN_NAME is already running"
         exit 1
     }
-    $PROJECT_PATH/$BIN_NAME &
+
+    eval "$COMMAND" &
 }
 
 stop(){
-    status | grep -q stopped && {
-        echo "error: $BIN_NAME was already stopped, no need to stop again."
-        exit 1
+    [[ "$STATUS" = "stopped" ]] && {
+        echo "warning: $BIN_NAME was already stopped"
+        return
     }
+
     kill -SIGTERM "$PID" && echo "success: graceful stopped"
 }
 
 reload(){
-    status | grep -q stopped && {
-        echo "error: no $BIN_NAME process found, please start first."
+    [[ "$STATUS" = "stopped" ]] && {
+        echo "warning: no $BIN_NAME process found"
         exit 1
     }
+
     kill -SIGHUP "$PID" && echo "success: graceful reload"
 }
 
 main(){
     usage "$@"
+
+    cd $PROJECT_PATH || exit 1
+
+    status &>/dev/null
 
     case $1 in
         start) start
